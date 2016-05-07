04becc5366c15ff72174469d73a9d7b246a35717
Augustus Mayo <gusmayo@gmail.com>
2016-05-07 12:57:00 -0500

    Start on champ select

---
+++ src/js/model/freelancer.js
@@ -0,0 +1,28 @@
+module.exports = (function() {
+
+  var privateNS = {
+    availableFreelancers: [
+      'asana',
+      'aurora',
+      'elle',
+      'garrison',
+      'gremolitionsinc',
+      'grey',
+      'helio',
+      'lockwood',
+      'nix',
+      'oz',
+      'pup',
+      'quark',
+      'rampart',
+      'rask',
+      'titus',
+      'zuki'
+    ]
+  };
+
+  function Freelancer(name) {
+  }
+
+  return Freelancer;
+})();
